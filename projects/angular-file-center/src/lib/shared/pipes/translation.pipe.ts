06a57200806fe2e7055003296ec4da1f24e45bae
Aron <aron886tw@gmail.com>
2021-02-21 08:10:54 +0800

    fix: import

--- projects/angular-file-center/src/lib/shared/pipes/translation.pipe.ts
+++ projects/angular-file-center/src/lib/shared/pipes/translation.pipe.ts
@@ -1,5 +1,5 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import { FileService } from '../file.service';
+import { FileService } from '../../file.service';
 
 @Pipe({
     name: 'translation'
