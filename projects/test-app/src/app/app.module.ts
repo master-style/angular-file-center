65e2e57b4db4bbbf649148a36c488da8c418d7dd
miles0930 <swrev5741@gmail.com>
2021-02-21 20:30:19 +0800

    add test app

---
+++ projects/test-app/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { FileModule } from '../../../angular-file-center/src/public-api';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    FileModule.forRoot()
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
