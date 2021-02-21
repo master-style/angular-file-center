3ea2d36731a88612690c09b1af98a667d61c78d7
Aron <aron886tw@gmail.com>
2021-02-21 11:38:10 +0800

    fix: route

--- projects/angular-file-center/src/lib/file.component.ts
+++ projects/angular-file-center/src/lib/file.component.ts
@@ -40,13 +40,14 @@ export class FileComponent implements OnInit, OnDestroy, AfterViewInit {
 
                 this.fileService.directoryPaths = directoryPaths;
             });
+    }
+
+    ngAfterViewInit(): void {
 
         if (!this.route.snapshot.firstChild) {
             this.fileService.onDirectoryChanged.next(null);
         }
-    }
-
-    ngAfterViewInit(): void {
+        
         this.modalRef?.nativeElement.open();
     }
 
