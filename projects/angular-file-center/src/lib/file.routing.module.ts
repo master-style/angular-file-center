3b59116cbe1fdf5cf5295c1ca7b59f0cf49cc3c6
Aron <aron886tw@gmail.com>
2021-02-21 09:49:33 +0800

    feat: export fileRouteConfig

--- projects/angular-file-center/src/lib/file.routing.module.ts
+++ projects/angular-file-center/src/lib/file.routing.module.ts
@@ -2,17 +2,19 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FileComponent } from './file.component';
 
+export const fileRouteConfig = {
+    path: 'file',
+    component: FileComponent,
+    children: [
+        {
+            path: ':id',
+            loadChildren: () => import('./directory/directory.module').then(m => m.DirectoryModule)
+        }
+    ]
+};
+
 const routes: Routes = [
-    {
-        path: 'file',
-        component: FileComponent,
-        children: [
-            {
-                path: ':id',
-                loadChildren: () => import('./directory/directory.module').then(m => m.DirectoryModule)
-            }
-        ]
-    },
+    fileRouteConfig
 ];
 
 @NgModule({
