07a94adc7c6b3d897ea80e71545c88f6fdca56e9
Aron <aron886tw@gmail.com>
2021-02-22 19:38:57 +0800

    fix: build

--- projects/angular-file-center/src/public-api.ts
+++ projects/angular-file-center/src/public-api.ts
@@ -2,12 +2,19 @@
  * Public API Surface of angular-file-center
  */
 
-export * from './lib/file.routes';
 export * from './lib/file.service';
 export * from './lib/file.module';
+export * from './lib/file.component';
+export * from './lib/file.routes';
+export * from './lib/directory/directory-routing.module';
+export * from './lib/directory/directory.component';
+export * from './lib/directory/directory.module';
 export * from './lib/shared/utils/get-handler-from-firebase-storage';
 export * from './lib/shared/utils/convert-image';
 export * from './lib/shared/utils/is-accepted-file';
 export * from './lib/shared/utils/is-image-file-name';
 export * from './lib/shared/utils/load-image-from-file';
 export * from './lib/shared/utils/read-as-data-url';
+export * from './lib/shared/pipes/file-metadata.pipe';
+export * from './lib/shared/pipes/file-url.pipe';
+export * from './lib/shared/pipes/translation.pipe';
